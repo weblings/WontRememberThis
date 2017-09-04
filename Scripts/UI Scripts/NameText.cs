bc3d2257e07e41de510443240201c099714e129d
weblings <weblings.lings@gmail.com>
2017-09-04 16:19:06 -0700

    Makes player-controlled Faction name appear on screen

---
+++ Scripts/UI Scripts/NameText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameText : MonoBehaviour {
+
+	string name;
+	public Text text;
+	GameScript game;
+
+	// Use this for initialization
+	void Start () {
+		game = GameObject.FindWithTag ("GameController").GetComponent<GameScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		name = game.factions[0].name;
+		text.text = name;
+	}
+
+}
